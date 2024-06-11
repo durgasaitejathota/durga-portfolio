85da70a07b3aba2438ece49be4e58690122479ac
durgasaitejathota <112675470+durgasaitejathota@users.noreply.github.com>
2024-06-11 18:40:23 -0500

    Update testimonials.js

--- testimonials.js
+++ testimonials.js
@@ -1,9 +1,9 @@
 const testimonials = [
   {
     id: 1,
-    name: 'Shaif Arfan',
-    title: 'CEO',
-    org: 'WEB CIFAR',
+    name: 'Sundar Narayana',
+    title: 'Team Lead',
+    org: 'Tata Consultancy Services',
     desc:
       'Durga is a person of commitment. He is really good at what he is doing. I really like his work.',
   },
@@ -11,6 +11,7 @@ const testimonials = [
     id: 2,
     name: 'Savitha Dongre',
     title: 'Project Manager',
+    org: 'Tata Consultancy Services',
     desc:
       'Really amazing communication skills. Always understand what I am trying to achieve. Also, his work is really amazing. Do really high-quality work.',
   },
@@ -18,6 +19,7 @@ const testimonials = [
     id: 3,
     name: 'Navya',
     title: 'Team Member',
+    org: 'Tata Consultancy Services',
     desc:
       'Really hard-working person. Deliver the work as promised. Planning to work more with this person.',
   },
