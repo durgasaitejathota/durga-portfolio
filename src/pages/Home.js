ca59ef8f0507b1914c20a6a0fe325fec0ce909f4
durgasaitejathota <112675470+durgasaitejathota@users.noreply.github.com>
2024-10-16 17:32:29 -0500

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,15 @@ import ProjectsSection from '../components/ProjectsSection';
 
 export default function Home() {
   return (
+     <div>
+      <HeroSection />
+      <div>
+        <h2>Let Me Introduce Myself</h2>
+        <p>I'm Durga Sai Teja Thota, a Java Full Stack Developer with 3.7 years of experience in building scalable web applications and microservices.
+    I specialize in Java, Spring Boot, React, and cloud technologies like AWS, with skills in Docker, Kubernetes, and database optimization. 
+    I have a strong background in microservices, CI/CD automation, and Agile collaboration, delivering high-quality software solutions across various projects.</p>
+        <button>More About Me</button> {/* Add this button */}
+      </div>
     <div>
       <HeroSection />
       <ExperienceSection /> {/* Include the Experience Section */}
