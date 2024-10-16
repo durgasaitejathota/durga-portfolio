de3a758d4dd50fa13273c6f9a153b76e4b318617
durgasaitejathota <112675470+durgasaitejathota@users.noreply.github.com>
2024-10-16 15:58:17 -0500

    Update projects.js

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -5,28 +5,29 @@ import RMSImg from '../images/RMS.jpg';
 import ACGImg from '../images/ACG.jpg';
 //import CavinImg from '../images/cavinimg.jpg';
 
-const projects = [
+export const projects = [
   {
-    id: uuidv4(),
-    name: 'Project Monitoring System',
-    desc:
-      'The Project Monitoring System offers real-time task tracking and progress visualization, fostering transparency and proactive decision-making. It incorporates role-based access control and notification features to enhance communication channels, keeping stakeholders informed about project updates and milestones efficiently.',
-    img: PMSImg,
+    id: 1,
+    name: 'Automatic Generation of Image Captions Using Deep Learning',
+    description: 'Developed advanced image captioning algorithms using deep learning frameworks like CNNs and LSTMs, achieving a 35% improvement in caption accuracy.',
+    image: '/assets/images/ACG.jpg',
+    link: '/projects/automatic-image-captioning'
   },
   {
-    id: uuidv4(),
-    name: 'Restuarant Management System',
-    desc:
-      'The Restaurant Management System features a customizable menu management module for easy updates and item categorization. It also includes a reservation management system with dynamic table availability tracking and automated confirmation notifications, streamlining operations and improving customer satisfaction.',
-    img: RMSImg,
+    id: 2,
+    name: 'Restaurant Management System',
+    description: 'Implemented a comprehensive management solution using Java 8 and Spring Boot, including modules for menu management and order tracking.',
+    image: '/assets/images/RMS.jpg',
+    link: '/projects/restaurant-management'
   },
   {
-    id: uuidv4(),
-    name: 'Atomatic Generation Image Captions based on Deep Learning and Neural Network:',
-    desc:
-      'The Automatic Generation Image Captions system employs advanced image captioning algorithms developed in Java, utilizing Convolutional Neural Networks (CNNs) and Long Short-Term Memory (LSTM) networks via Deeplearning4j.',
-    img: ACGImg,
-  },
+    id: 3,
+    name: 'Sales Data Analysis and Visualization for Sample Superstore',
+    description: 'Utilized Power BI for cleaning and visualizing sales data, creating interactive dashboards to analyze product performance.',
+    image: '/assets/images/SalesData.jpg',
+    link: '/projects/sales-data-visualization'
+  }
 ];
+
 
 export default projects;
