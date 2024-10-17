1ad02380886d3902d69e05b0cfe18a34f772751d
durgasaitejathota <112675470+durgasaitejathota@users.noreply.github.com>
2024-10-17 17:59:32 -0500

    Update projects.js

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -16,21 +16,21 @@ export const projects = [
     id: uuidv4(),
     name: 'Sapot Systems Inc.',
     description: 'Java Full Stack Developer (July 2023 – Present). Led the development of microservices for Load Configuration and Shift Scheduling, enhancing system scalability by 30%. Integrated a chatbot for dynamic shift management, increasing scheduling accuracy by 25%. Automated testing with JUnit and Mockito, achieving 95% code coverage.',
-    image: '/assets/images/SapotSystems.jpg', // Update the image path as needed
+    image: '/assets/images/SapotSystemsProject.jpg', // Updated image path
     link: '/projects/sapot-systems'
   },
   {
     id: uuidv4(),
     name: 'Tata Consultancy Services',
     description: 'Software Developer (May 2021 – July 2022). Developed a content delivery optimization platform, boosting media retrieval speed by 40%. Enhanced API security using Spring Security and OAuth2, ensuring safe data exchanges. Automated cloud deployments on AWS ECS, achieving 99.9% system uptime.',
-    image: '/assets/images/TCS.jpg', // Update the image path as needed
+    image: '/assets/images/TCSProject.jpg', // Updated image path
     link: '/projects/tcs'
   },
   {
     id: uuidv4(),
     name: 'Zoron IT Solutions LLP',
     description: 'Java Full Stack Developer (July 2019 – May 2021). Led the development of an Inventory and Order Management System, automating workflows and reducing manual intervention by 40%. Optimized data storage using MongoDB, improving query response time by 25%. Implemented JWT-based authentication for secure API access.',
-    image: '/assets/images/ZoronIT.jpg', // Update the image path as needed
+    image: '/assets/images/ZoronITProject.jpg', // Updated image path
     link: '/projects/zoron-it'
   }
 ];
